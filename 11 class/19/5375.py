90b2515ca919b2d2641580431e52907525125b63
student <4@fg>
2024-12-23 11:48:18 +1200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	10 класс/21,11,2024/2.py
    #	10 класс/21,11,2024/3.py
    #	10 класс/21,11,2024/7.py

---
+++ 11 class/19/5375.py
@@ -0,0 +1,10 @@
+def game(s,step):
+    if s >= 165:
+        return step % 2 == 0
+    if step == 0:
+        return 0
+    h = [game(s+1, step-1), game(s*2,step-1)]
+    return any(h) if (step-1) % 2 == 0 else all(h)
+print('19', [s for s in range(1,165) if game(s,2)])
+print('20', [s for s in range(1,165) if game(s,3) and not game(s,1)])
+print('21', [s for s in range(1,165) if game(s,4) and not game(s,2)])
